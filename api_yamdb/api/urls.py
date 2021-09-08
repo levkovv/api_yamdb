84c4a47b74d53aab75af6627bad9fcc9c99dcae4
Vlad <levkovv13@yandex.ru>
2021-09-08 21:20:01 +0300

    Сделаны сериалайзеры, вьюсеты, роутеры

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import TitleViewSet, CategoryViewSet, GenreViewSet
+
+
+router = DefaultRouter()
+router.register(r'titles', TitleViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
 
 urlpatterns = [
-	
+	path('v1/', include(router.urls)),
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
-    'users'
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +107,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
