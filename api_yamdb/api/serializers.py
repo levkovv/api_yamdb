aa874be5bc676afd204f804d34fe3981833a04f3
Vlad <levkovv13@yandex.ru>
2021-09-10 11:50:44 +0300

    Доделал вьюхи, сериализаторы, добавил пермишен

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -5,29 +5,29 @@ from reviews.models import Category, Genre, Title, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = Category
-		fields = ('name', 'slug')
+    
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
 
 
 class GenreSerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = Genre
-		fields = ('name', 'slug')
+    
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
 
 
 class TitleSerializer(serializers.ModelSerializer):
-	rating = serializers.SerializerMethodField()
-	genre = GenreSerializer(many=True)
-	category = serializers.SlugRelatedField(slug_field='name',
-		queryset=Category.objects.all())
-	
-	class Meta:
-		model = Title
-		fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
-	
-	def get_rating(self, obj): # Здесь нужно будет высчитывать rating из score в БД
-		return 100
-		
+    rating = serializers.SerializerMethodField()
+    genre = GenreSerializer(many=True, read_only=True)
+    category = serializers.SlugRelatedField(slug_field='slug',
+        read_only=True)
+    
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
+    
+    def get_rating(self, obj): # Здесь нужно будет высчитывать rating из score в БД
+        return 100
+ 
